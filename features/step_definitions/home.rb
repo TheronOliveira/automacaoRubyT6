070dbad436ae137f830d787292b1664a80b1359d
TheronOliveira <45536620+TheronOliveira@users.noreply.github.com>
2022-08-15 22:29:36 -0300

    adicionando métodos na classe home_page

--- features/step_definitions/home.rb
+++ features/step_definitions/home.rb
@@ -1,7 +1,7 @@
-Dado('que eu informe a {string} da página home do projeto Qa.Coders') do |string|
-    home.load
-  end
-  
-  Entao('deve ser visualizada a mensagem {string}') do |titleHomePage|
-    home.validate_text_homePage(titleHomePage)
-  end
+Dado('que eu informe a url da página home do projeto Qa.Coders') do
+  home.load
+end
+
+Entao('deve ser visualizada a mensagem {string}') do |titleHomePage|
+  home.validate_text_homePage(titleHomePage)
+end
